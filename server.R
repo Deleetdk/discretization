6caae906321f2f8c1d17b5f07b49db24ef7daec2
Emil O. W. Kirkegaard <emil@emilkirkegaard.dk>
2015-06-14 23:55:34 +0200

    first release

---
+++ server.R
@@ -0,0 +1,54 @@
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+library(MASS)
+library(ggplot2)
+library(stringr)
+
+shinyServer(function(input, output) {
+  
+  reac_data = reactive({
+    #generate data
+    d = as.data.frame(mvrnorm(10000, c(0, 0), matrix(c(1, input$pop_cor, input$pop_cor, 1), ncol = 2), empirical = T))
+    colnames(d) = c("A", "B")
+    
+    #cut A
+    if ("A" %in% input$cut) { #to cut or not
+      if ("A" %in% input$method) { #how to cut
+        d$A = cut(d$A, quantile(d$A, seq(0, 1, length.out = input$A_bins + 1)), labels = F, include.lowest = T)
+      } else {d$A = cut(d$A, input$A_bins, labels = F)}
+    }
+    
+    #cut B
+    if ("B" %in% input$cut) {
+      if ("B" %in% input$method) {
+        d$B = cut(d$B, quantile(d$B, seq(0, 1, length.out = input$B_bins + 1)), labels = F, include.lowest = T)
+      } else {d$B = cut(d$B, input$B_bins, labels = F)}
+    }
+    
+    return(d)
+  })
+
+  output$distPlot <- renderPlot({
+    #jittering
+    if (input$jitter) {position = position_jitter(w = 0.1, h = 0.1)} else {position = "identity"}
+    
+    #data
+    d = reac_data()
+    
+    #plot
+    ggplot(d, aes(A, B)) +
+      geom_point(position = position, alpha = .3) +
+      geom_smooth(method = "lm", se = F) +
+      annotate("text",
+               x = mean(d$A), y = mean(d$B),
+               label = str_c("Correlation in sample: ", round(cor(d$A, d$B), 3)),
+               color = "darkorange", size = 10)
+  })
+
+})
---
+++ ui.R
@@ -0,0 +1,49 @@
+
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+
+shinyUI(fluidPage(
+
+  # Application title
+  titlePanel("Discretization"),
+
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("pop_cor",
+                  "Underlying population correlation.",
+                  min = 0,
+                  max = 1,
+                  value = .5),
+      sliderInput("A_bins",
+                  "Number of bins for variable A.",
+                  min = 2,
+                  max = 15,
+                  value = 5),
+      sliderInput("B_bins",
+                  "Number of bins for variable B.",
+                  min = 2,
+                  max = 15,
+                  value = 10),
+      checkboxGroupInput("method", "Use equal sized bins instead of equal ranged bins:",
+                         c("Variable A" = "A",
+                           "Variable B" = "B")),
+      checkboxGroupInput("cut", "Discretize:",
+                         c("Variable A" = "A",
+                           "Variable B" = "B"), selected = "A"),
+      checkboxInput("jitter", "Jitter points to avoid overplotting?", T)
+    ),
+
+    # Show a plot of the generated distribution
+    mainPanel(
+      HTML("Sometimes we are trying to measure something but only have a measure with a few possible values. This could be social classes or mental traits where we use a very short meaurement (e.g. 5 yes/no questions scored at 1 or 0). In practice this means that the correlations based on such data will be biased towards zero. This visualization helps you get a feel for how this works. Try playing around with the options to the left and see the effects. The bias is stronger when both variables are discretized and when they use a small number of possible values."),
+      plotOutput("distPlot"),
+      HTML("Made by <a href='http://emilkirkegaard.dk'>Emil O. W. Kirkegaard</a> using <a href='http://shiny.rstudio.com/'/>Shiny</a> for <a href='http://en.wikipedia.org/wiki/R_%28programming_language%29'>R</a>. Source code available on <a href='https://github.com/Deleetdk/discretization'>Github</a>.")
+    )
+  )
+))
